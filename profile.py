5985c0df012249c35680017cd68cfd10e85641b3
Pradeep987654321 <105903974+Pradeep987654321@users.noreply.github.com>
2024-08-20 21:31:55 +0530

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from ydata_profiling import ProfileReport
-from io import BytesIO
+import tempfile
 
 # Streamlit file uploader
 st.title("CSV File Uploader and EDA Report")
@@ -15,15 +15,16 @@ if uploaded_file is not None:
         # Generate the profiling report
         profile = ProfileReport(df, minimal=True)
         
-        # Save the profiling report to a BytesIO object
-        with BytesIO() as buffer:
-            profile.to_file(buffer)
-            buffer.seek(0)
-            
-            # Provide a download link for the HTML file
+        # Save the profiling report to a temporary file
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".html") as tmp_file:
+            profile.to_file(tmp_file.name)
+            tmp_file_path = tmp_file.name
+        
+        # Provide a download link for the HTML file
+        with open(tmp_file_path, "rb") as f:
             st.download_button(
                 label="Download Report",
-                data=buffer,
+                data=f,
                 file_name="profile_report.html",
                 mime="text/html"
             )
